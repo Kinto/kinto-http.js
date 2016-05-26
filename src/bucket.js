5d9aff09b2421ab3225938ebfb873941255e02e7
Mathieu Leplatre <mathieu@mozilla.com>
2016-05-26 16:24:07 +0200

    Leftover

--- src/bucket.js
+++ src/bucket.js
@@ -126,7 +126,7 @@ export default class Bucket {
    * @param  {Boolean} options.safe        The safe option.
    * @param  {Object}  options.headers     The headers object option.
    * @param  {Object}  options.permissions The permissions object.
-   * @param  {Object}  options.data        The metadadata object.
+   * @param  {Object}  options.data        The data object.
    * @param  {Object}  options.schema      The JSONSchema object.
    * @return {Promise<Object, Error>}
    */
