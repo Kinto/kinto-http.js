192b1d89c15934f79c01af7a0c23cfe505b6f23c
Dylan Staley <staley.dylan@gmail.com>
2019-06-27 14:20:34 -0700

    remove browserify hack from entry point

--- src/index.js
+++ src/index.js
@@ -11,10 +11,3 @@ export default class KintoClient extends KintoClientBase {
     super(remote, Object.assign({ events }, options));
   }
 }
-
-// This is a hack to avoid Browserify to expose the above class
-// at `new KintoClient()` instead of `new KintoClient.default()`.
-// See https://github.com/Kinto/kinto-http.js/issues/77
-if (typeof module === "object") {
-  module.exports = KintoClient;
-}
