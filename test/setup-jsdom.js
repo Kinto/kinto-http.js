9f6e10253e711ed859edf8366dbf7ffc98960da2
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-07-23 16:48:13 -0400

    If Blob is not present, use Buffer
    This should help us run in NodeJS environments.

--- test/setup-jsdom.js
+++ test/setup-jsdom.js
@@ -13,9 +13,8 @@ global.HTMLElement = window.HTMLElement;
 // Expose a global fetch polyfill
 global.fetch = global.window.fetch = require("isomorphic-fetch");
 
-// jsdom FormData & Blob implementations are inconsistent, exposing better ones
+// jsdom FormData implementation is inconsistent, exposing a better one
 global.FormData = require("form-data");
-global.Blob = sequences => Buffer.from(sequences[0]);
 
 // atob & btoa polyfill for tests
 global.atob = require("atob");
