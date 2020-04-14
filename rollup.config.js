b710f255bf21d2b2913ccfa51a9421356ee8e949
Dylan Staley <88163+dstaley@users.noreply.github.com>
2020-04-13 19:00:27 -0700

    move testing rollup config to main rollup file

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import inject from "@rollup/plugin-inject";
 import { terser } from "rollup-plugin-terser";
+import multi from "@rollup/plugin-multi-entry";
+import replace from "@rollup/plugin-replace";
 
 const geckoBuild = {
   input: "./src/index.fx.ts",
@@ -78,4 +80,48 @@ const nodeBuild = {
   ],
 };
 
-export default [geckoBuild, browserBuild, nodeBuild];
+const browserTestBuild = {
+  input: "./test/*_test.ts",
+  output: [
+    {
+      file: "dist/test-suite.js",
+      format: "iife",
+      sourcemap: true,
+      globals: {
+        intern: "intern",
+      },
+    },
+  ],
+  plugins: [
+    multi(),
+    resolve({
+      mainFields: ["browser", "module", "main"],
+      preferBuiltins: true,
+    }),
+    typescript({
+      target: "es2019",
+      types: ["intern"],
+    }),
+    replace({
+      __dirname: JSON.stringify(path.join(__dirname, "test")),
+      "process.env.TEST_KINTO_SERVER": JSON.stringify(
+        process.env.TEST_KINTO_SERVER ? process.env.TEST_KINTO_SERVER : ""
+      ),
+      "process.env.SERVER": JSON.stringify(
+        process.env.SERVER ? process.env.SERVER : ""
+      ),
+      "process.env.KINTO_PROXY_SERVER": JSON.stringify(
+        process.env.SERVER ? process.env.SERVER : "http://localhost:8899"
+      ),
+      "http://0.0.0.0": "http://localhost",
+    }),
+    builtins(),
+    commonjs(),
+  ],
+};
+
+const bundles = process.env.BROWSER_TESTING
+  ? [browserTestBuild]
+  : [geckoBuild, browserBuild, nodeBuild];
+
+export default bundles;
